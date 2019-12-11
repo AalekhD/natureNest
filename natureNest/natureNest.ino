3ff1ff5e3f3829b130972273915fe71b31925ab1
Aalakh <alekhdevari@gmail.com>
2019-12-11 09:32:19 +0530

    Documents added

--- natureNest/natureNest.ino
+++ natureNest/natureNest.ino
@@ -1,5 +1,22 @@
-void setup() {
+/*
+ *  Project Name : nature Nest
+ *  Description  : Project is for a Resort named Nature Nest
+ *  Requirement  : A button press should illuminate the LED respective
+ *                 to the bird button pressed and sound of that particular
+ *                 bird should be played
+ *                 This should be replicated for 15 such birds
+ *                 For Demo purpose only 5 birds will be shown
+ * 
+ */
 
+/* Button Input Pins*/
+buttonInputs = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+/* Relay Output Pins */
+relayOutput = [20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35];
+
+void setup() {
+  
 }
 
 void loop() {
